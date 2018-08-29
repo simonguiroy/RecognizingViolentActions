a7d460d1487f70f1375c78dd298dbece35beaf7d
simonguiroy <guiroysi@elisa1.iro.umontreal.ca>
2018-08-28 20:24:59 -0400

    added subset of data to produce statistics on performance

--- datasets/video_dataset.py
+++ datasets/video_dataset.py
@@ -9,6 +9,8 @@ from preprocessing import preprocess_rgb, preprocess_flow
 from torch.utils.data import DataLoader
 
 
+# For now, this dataset class should only be used to evaluate a model, and not for training
+
 class VideoDataset(Dataset):
     """Dataset of video sequences, pre-processed as Torch tensors for rgb and optical-flow stream"""
 
@@ -16,13 +18,15 @@ class VideoDataset(Dataset):
                  csv_file, 
                  root_dir,
                  stream='rgb',
+                 split='train',
                  transform=None):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
+            root_dir (string): Directory with all the video files.
+            stream (string): Type of stream for model input [rgb/flow/joint]
+            split (string): Split of the dataset to take samples from [train/valid/test/all]
+            transform (callable, optional): Optional transform to be applied after preprocessing, like random croping.
         """
         self.csv_data = genfromtxt(csv_file, delimiter=',', dtype=str)
         self.root_dir = root_dir
@@ -34,13 +38,13 @@ class VideoDataset(Dataset):
         
         
     def __getitem__(self, idx):
-        video_id = self.csv_data[idx][0]
+        video_filename = self.csv_data[idx][0]
         label = self.csv_data[idx][1]
         sample = {'videos': {'video_rgb': torch.zeros(224, 224), 'video_flow': torch.randn(224, 224)}, 'label': label}
         
         if self.stream == 'rgb' or self.stream == 'joint':
-            sample['videos']['video_rgb'] = torch.from_numpy(preprocess_rgb(self.root_dir + video_id + '.mp4'))
+            sample['videos']['video_rgb'] = torch.from_numpy(preprocess_rgb(self.root_dir + video_filename))
         if self.stream == 'flow' or self.stream == 'joint':
-            sample['videos']['video_flow'] = preprocess_flow(self.root_dir + video_id + '.mp4')
+            sample['videos']['video_flow'] = preprocess_flow(self.root_dir + video_filename)
             
         return sample
